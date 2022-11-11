9d25eb7353db85400f5a41487ccabd415468b875
sanjuny <sanjuny07@gmail.com>
2022-11-12 00:08:52 +0530

    sidebar design compelete

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import Loginpage from './pages/Loginpage';
 import Signuppage from './pages/Signuppage';
 import HomePages from './pages/HomePages';
 import Formpage from './pages/Formpage';
+import Sidebarpage from './pages/Admin/Sidebarpage';
+import Adminloginpage from './pages/Admin/Adminloginpage';
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-       <Routes>
 
+       <Routes>
         <Route path='/login' element={<Loginpage />}/>
         <Route path='/signup' element={<Signuppage />}/> 
         <Route path='/homepage' element={<HomePages />}/>
         <Route path='/form' element={<Formpage />}/>
-        
        </Routes>
+
+       <Routes>
+        <Route path='/dashboard' element={<Sidebarpage />}/>
+        <Route path='/adminlogin' element={<Adminloginpage />}/>
+       </Routes>
+
+      
       </Router>
       
     </div>
---
+++ client/src/pages/Admin/Adminloginpage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminLogin from '../../components/admin/AdminLogin/AdminLogin'
+
+function Adminloginpage() {
+  return (
+    <div>
+      <AdminLogin />
+    </div>
+  )
+}
+
+export default Adminloginpage
